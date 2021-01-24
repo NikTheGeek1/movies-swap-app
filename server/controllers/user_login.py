7a09e3256abbe4018c148dd703da7732628d54dc
nikos theodoropoulos <ntheodoropoulos@outlook.com>
2021-01-24 15:40:35 +0200

    preparing for deploy

--- server/controllers/user_login.py
+++ server/controllers/user_login.py
@@ -1,7 +1,8 @@
 from flask import Flask, Blueprint
+from json import loads, dumps
 
 user_login_blueprint = Blueprint('user_login', __name__)
 
 @user_login_blueprint.route('/')
 def user_login():
-    pass
+    return dumps({"success": True}), 200, {'Content-type': 'application/json'}
