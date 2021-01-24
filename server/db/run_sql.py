e467cdae24dc0ba303a01ae9692de1917fdd8e32
nikos theodoropoulos <ntheodoropoulos@outlook.com>
2021-01-24 18:10:45 +0200

    updates

--- server/db/run_sql.py
+++ server/db/run_sql.py
@@ -1,12 +1,31 @@
 import psycopg2
 import psycopg2.extras as ext
+import urllib.parse as urlparse
+import os
+
+url = urlparse.urlparse(os.environ["DATABASE_URL"])
+print(url)
+dbname = url.path[1:]
+user = url.username
+password = url.password
+host = url.hostname
+port = url.port
+
+
+
 
 def run_sql(sql, values = None):
     conn = None
     results = []
 
     try:
-        conn=psycopg2.connect("dbname='movies-swap'")
+        conn = psycopg2.connect(
+            dbname=dbname,
+            user=user,
+            password=password,
+            host=host,
+            port=port
+            )
         cur = conn.cursor(cursor_factory=ext.DictCursor)
         cur.execute(sql, values)
         conn.commit()
